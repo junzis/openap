382f060f2c37f0f65755a1712f4b4f84e2254977
VincentMeijer <vincentrmeijer@gmail.com>
2024-03-08 16:33:20 +0100

    More typos

--- openap/extra/aero.py
+++ openap/extra/aero.py
@@ -7,7 +7,7 @@ Functions for aeronautics in this module
 International Standard Atmosphere
     p,rho,T = atmos(h)    # atmos as function of geopotential altitude h [m]
     a = vsound(h)         # speed of sound [m/s] as function of h[m]
-    p = pressure(h)       # calls atmos but retruns only pressure [Pa]
+    p = pressure(h)       # calls atmos but returns only pressure [Pa]
     T = temperature(h)    # calculates temperature [K]
     rho = density(h)      # calls atmos but returns only density [kg/m3]
 
@@ -15,11 +15,11 @@ Speed conversion at altitude h[m] in ISA:
     mach = tas2mach(v_tas,h)    # true airspeed (v_tas) to mach number conversion
     v_tas = mach2tas(mach,h)    # true airspeed (v_tas) to mach number conversion
     v_tas = eas2tas(v_eas,h)    # equivalent airspeed to true airspeed, h in [m]
-    v_eas = tas2eas(v_tas,h)    # true airspeed to equivent airspeed, h in [m]
+    v_eas = tas2eas(v_tas,h)    # true airspeed to equivalent airspeed, h in [m]
     v_tas = cas2tas(v_cas,h)    # v_cas  to v_tas conversion both m/s, h in [m]
     v_cas = tas2cas(v_tas,h)    # v_tas to v_cas conversion both m/s, h in [m]
     v_cas = mach2cas(mach,h)    # mach to v_cas conversion v_cas in m/s, h in [m]
-    mach   = cas2mach(v_cas,h)  # v_cas to mach copnversion v_cas in m/s, h in [m]
+    mach   = cas2mach(v_cas,h)  # v_cas to mach conversion v_cas in m/s, h in [m]
 """
 import numpy as np
 import scipy.constants
